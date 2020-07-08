4e741232e55bce7d5f701c189fb6736770332ce9
Chris Sipe <csipe24@gmail.com>
2020-07-07 17:51:41 -0700

    Added HandleBars Content

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,14 +2,14 @@ const router = require("express").Router();
 const Burger = require("../models/burger");
 
 router.get("/", (req, res)=>{
-    Burger.all("*", (results) => {
+    Burger.all("*", (results) => {  
     res.render("index", {
         burgers: results
+        });
     });
 });
-});
 
-router.post("/api/burgers", () =>{
+router.post("/api/burgers", (req, res) =>{
     Burger.create(req.body, (result)=>{
         res.json({id: result.insertId});
     });
